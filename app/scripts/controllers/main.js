18054f9b2cd3fd76f7e00b824d655bcdb730b5ea
Siddharth Venkatesan <neotamizhan@gmail.com>
2015-10-12 16:41:06 +0300

    adding bmi stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -29,36 +29,29 @@ angular.module('healthTrackerApp')
       user.$save();
     }
 
-    $scope.open = function (user) {
-    $scope.user = user;
-                  $modal.open({
-                      templateUrl: 'entryContent.html',
-                      backdrop: true,
-                      windowClass: 'modal',
-                      controller: function ($scope, $modalInstance, $log, user) {
-                          $scope.entry = {date:new Date().toISOString().slice(0, 10), weight:0};
-                          $scope.user = user;
-                          $scope.submit = function () {
-                              $log.log('Submiting user info.');
-                              $log.log(JSON.stringify($scope.entry));
-                              $log.log(JSON.stringify($scope.user));
-                              $scope.user.entries.push($scope.entry)
-                              $scope.user.$save();
-                              $modalInstance.dismiss('cancel');
-                          }
+    $scope.initialBmi = function (user) {
+        var entry = user.entries[0];
+        return $scope.GetBmi(user, entry);
+    }
 
-                          $scope.cancel = function () {
-                              $modalInstance.dismiss('cancel');
-                          };
-                      },
+    $scope.currentBmi = function (user) {
+        var entry = user.entries[user.entries.length-1];
+        return $scope.GetBmi(user, entry);
+    }
 
-                      resolve: {
-                          user: function () {
-                              return $scope.user;
-                          }
-                      }
-                  });
-                }
+    $scope.bmiNetDiff = function (user) {
+      return $scope.initialBmi(user) - $scope.currentBmi(user);
+    }
+
+    $scope.getBmiColor = function (bmi) {
+      var color = "";
+
+      if (bmi >= 18.5 && bmi <= 24.9) {color = "success"}
+      if (bmi >= 25 && bmi <= 29.9) {color = "warning"}
+      if (bmi >= 30) {color = "danger"}
+
+      return color;
+    }
 
     $scope.FetchAllUsers();
   });
