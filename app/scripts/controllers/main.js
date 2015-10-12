f86a614cff25099b7e2a4f849a32972d1d9f37e6
Siddharth Venkatesan <siddhu.v@gmail.com>
2015-10-12 23:38:29 +0300

    added google signin feature

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,6 +16,10 @@ angular.module('healthTrackerApp')
         })
     }
 
+    $scope.isLoggedUser = function (user) {
+      return $scope.logged_email == user.id;
+    }
+
     $scope.GetBmi = function (user, entry) {
       if (!entry) return 0;
       var height_m = user.height / 100;
@@ -53,5 +57,81 @@ angular.module('healthTrackerApp')
       return color;
     }
 
-    $scope.FetchAllUsers();
+    $scope.signedIn = false;
+
+   // Here we do the authentication processing and error handling.
+   // Note that authResult is a JSON object.
+   $scope.processAuth = function(authResult) {
+       // Do a check if authentication has been successful.
+       console.log(authResult);
+       if(authResult['access_token']) {
+           // Successful sign in.
+           $scope.signedIn = true;
+           $scope.getUserInfo();
+           //     ...
+           // Do some work [1].
+           //     ...
+       } else if(authResult['error']) {
+           // Error while signing in.
+           $scope.signedIn = false;
+
+           // Report error.
+       }
+   };
+
+   $scope.processUserInfo = function(userInfo) {
+
+       // Or use his email address to send e-mails to his primary e-mail address.
+       $scope.logged_email = userInfo['emails'][0]['value'];
+
+       console.log($scope.logged_user);
+   }
+
+   // When callback is received, process user info.
+   $scope.userInfoCallback = function(userInfo) {
+       $scope.$apply(function() {
+           $scope.processUserInfo(userInfo);
+       });
+   };
+
+   // Request user info.
+   $scope.getUserInfo = function() {
+       gapi.client.request(
+           {
+               'path':'/plus/v1/people/me',
+               'method':'GET',
+               'callback': $scope.userInfoCallback
+           }
+       );
+   };
+
+   // When callback is received, we need to process authentication.
+   $scope.signInCallback = function(authResult) {
+       $scope.$apply(function() {
+           $scope.processAuth(authResult);
+       });
+   };
+    // Render the sign in button.
+    $scope.renderSignInButton = function() {
+        gapi.signin.render('signInButton',
+            {
+                'callback': $scope.signInCallback, // Function handling the callback.
+                'clientid': '503260251941-0ro1k4avns1u2hm321vh7n231urcr3mb.apps.googleusercontent.com', // CLIENT_ID from developer console which has been explained earlier.
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
+                                                                                  // as their explanation is available in Google+ API Documentation.
+                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
+                'cookiepolicy': 'single_host_origin'
+            }
+        );
+    }
+
+        // Start function in this example only renders the sign in button.
+    $scope.start = function() {
+      $scope.renderSignInButton();
+      $scope.FetchAllUsers();
+    };
+
+    // Call start function on load.
+    $scope.start();
+
   });
